d3b4f923976d50f6921ad1422dccbcb24c8456d5
diguifi <lumia-diguifi@hotmail.com>
2018-10-10 16:30:49 -0300

    Add item switcher

---
+++ src/objects/items/Switcher.ts
@@ -0,0 +1,22 @@
+export class Switcher extends Phaser.GameObjects.Sprite {   
+    private currentScene: Phaser.Scene;
+
+    constructor(params) {
+        super(params.scene, params.x, params.y, params.key);
+        this.currentScene = params.scene;
+
+        this.initPhysics();
+        this.currentScene.add.existing(this);  
+    }
+
+    public changeLocation(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    private initPhysics(): void{
+        this.currentScene.physics.world.enable(this);  
+        this.body.setBounce(0.1);
+        this.body.setCollideWorldBounds(true); 
+    } 
+}
--- src/scenes/initialScene.ts
+++ src/scenes/initialScene.ts
@@ -1,5 +1,6 @@
 import { Platform } from "../objects/Platform";
 import { Player } from "../objects/Player";
+import { Switcher } from "../objects/items/Switcher";
 
 const redColor = 0xff0000;
 const blueColor = 0x4286f4;
@@ -13,6 +14,9 @@ export class initialScene extends Phaser.Scene {
     private timerText: Phaser.GameObjects.Text;
     private roundTime: number;
     private initalRoundTime: number = 15;
+    private switcher: Switcher;
+    private itemPosition: number;
+    private itemPositions;
 
     constructor() {
         super({
@@ -21,6 +25,11 @@ export class initialScene extends Phaser.Scene {
     }
 
     init(): void{
+        this.itemPosition = 0;
+        this.itemPositions = [[450, 50],
+                              [220, 220],
+                              [730, 100],
+                              [410, 250]];
         this.platforms = this.add.group({ classType: Platform }); 
         this.playerOneTurn = true; 
     }
@@ -37,6 +46,8 @@ export class initialScene extends Phaser.Scene {
         this.addPlatform(200, 150);
         this.addPlatform(-200, 300);
         this.addPlatform(400, 450);
+        this.switcher = this.addSwitcher(this.itemPositions[this.itemPosition][0], 
+                                        this.itemPositions[this.itemPosition][1]);
         this.roundTime = this.initalRoundTime;
         this.player1 = this.addPlayer(1, 100, 200, redColor);
         this.player2 = this.addPlayer(2, 600, 300, blueColor);
@@ -66,6 +77,10 @@ export class initialScene extends Phaser.Scene {
         this.physics.collide(this.player2, this.platforms.getChildren());
         this.physics.overlap(this.player1, this.player2, this.playerCatch, null, this);  
         this.timerText.setText(' - ' + this.roundTime + ' - '); 
+
+        this.physics.collide(this.switcher, this.platforms.getChildren());
+        this.physics.overlap(this.player1, this.switcher, this.getSwitcher, null, this); 
+        this.physics.overlap(this.player2, this.switcher, this.getSwitcher, null, this); 
     }
 
     private playerCatch(): void{
@@ -86,6 +101,26 @@ export class initialScene extends Phaser.Scene {
         this.scoreText.setText('Player1: '+this.player1.score+'\nPlayer2: '+this.player2.score);
     }
 
+    private getSwitcher(player, switcher): void{
+        this.toggleTurn();
+
+        if(this.itemPosition + 1 < this.itemPositions.length){
+            this.itemPosition++;
+
+            switcher.changeLocation(this.itemPositions[this.itemPosition][0],
+                                    this.itemPositions[this.itemPosition][1]);
+        }
+        else {
+            
+            this.itemPosition = 0;
+
+            switcher.changeLocation(this.itemPositions[this.itemPosition][0],
+                                    this.itemPositions[this.itemPosition][1]);
+        }
+
+        
+    }
+
     private addPlatform(x, y): void{
         let platform = new Platform({
             scene: this,
@@ -94,6 +129,15 @@ export class initialScene extends Phaser.Scene {
             key: 'platform'
           });            
           this.platforms.add(platform);
+    }
+
+    private addSwitcher(x, y): Switcher{
+        return new Switcher({
+            scene: this,
+            x: x,
+            y: y,
+            key: 'item'
+          });
     }
 
     private addPlayer(playernumber, x, y, tint): Player{
